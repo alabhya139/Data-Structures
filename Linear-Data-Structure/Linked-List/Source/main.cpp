36420c2ff53f8ddbfc3b286d8697244db234a47a
Alabhya Pandey <alabhyap@gmail.com>
2017-07-12 15:16:55 +0530

    Update main.cpp
    updated the code by replacing if block with switch statements, for code readability!

--- Linear-Data-Structure/Linked-List/Source/main.cpp
+++ Linear-Data-Structure/Linked-List/Source/main.cpp
@@ -5,51 +5,80 @@ using namespace std;
 
 int main()
 {
-    Node* head=NULL;
+    Node *head=NULL;
     int data,position;
-    string choice;
-    here:
+    char choice;
+    char choice1;
+    string choice2;
     cout<<"                            Welcome Friend, Here you can do insertion in linked list"<<endl;
     cout<<"************************************************************************************************************************\n"<<endl;
     cout<<"Do you want to insert or delete anything(Yes or No):";
-    cin>>choice;
+    cin>>choice2;
     cout<<endl;
-    if(choice=="no")
+    if(choice2=="no")
     {
         goto en;
     }
-    while(choice!="no"||choice!="No"||choice!="NO")
+    while(choice2!="no"||choice2!="No"||choice2!="NO")
     {
-        if(choice=="Yes"||choice=="YES"||choice=="yes")
+        cout<<"What you want to perform(Insert or Delete or Exit), Press i for Insert, d for Delete x for exit():";
+        cin>>choice1;
+        cout<<endl;
+        switch(choice1)
         {
-                    cout<<"Where you want to insert(Beginning(beg) or Elsewhere(ew):";
+        case 'i' : {
+                   start: cout<<"Where you want to insert(Beginning(Press b) or Elsewhere(Press e):";
                     cin>>choice;
-               if(choice=="beg")
-                    {
-                        cout<<"Enter Data to insert:";
-                        cin>>data;
-                        insert_at_beggining(&head,data);
+                    switch(choice)
+                       {
+                       case 'b' :
+                           cout<<"Enter Data to insert:";
+                           cin>>data;
+                           insert_at_beggining(&head,data);
+                           break;
+                       case 'e' :
+                           cout<<"Enter the position where you want to insert:";
+                           cin>>position;
+                           cout<<endl;
+                           cout<<"Enter the Data you want to insert:";
+                           cin>>data;
+                           insert_after(&head,position,data);
+                           break;
                     }
-                else if(choice=="ew")
-                    {
-                        cout<<"Enter the position where you want to insert:";
-                        cin>>position;
-                        cout<<endl;
-                        cout<<"Enter the Data you want to insert:";
-                        cin>>data;
-                        insert_after(&head,position,data);
-                    }
-                cout<<"Want to insert some more data(Yes or No):";
-                cin>>choice;
-                cout<<endl;
-                if(choice=="no"||choice=="NO"||choice=="No")
-                {
-                    break;
-                }
-                else continue;
+                    cout<<"Want to insert some more data(Yes or No):";
+                    cin>>choice2;
+                    cout<<endl;
+                    if(choice2=="no"||choice2=="NO"||choice2=="No")
+                        {
+                           cout<<"Do you want to delete any item:";
+                           cin>>choice2;
+                           cout<<endl;
+                           if(choice2=="yes")
+                           {
+                               goto start1;
+                           }
+                        }
+                    else goto start;
+                  }
+         case 'd' : {
+                     start1:
+                     cout<<"Enter the position you want to perform delete on:";
+                     cin>>position;
+                     cout<<endl;
+                     delete_node(&head,position);
+                     cout<<"Want to delete more:";
+                     cin>>choice2;
+                     if(choice2=="yes")
+                     {
+                         goto start1;
+                     }
+                     break;
+                   }
+         case 'x' : goto en;
+         default:    cout<<"Invalid response, Try again!"<<endl;
         }
     }
-    cout<<"The Elements in the linked list are as follows:"<<endl;
     en:
+    cout<<"The Elements in the linked list are as follows:"<<endl;
     traverse_ll(head);
 }
